b0d00d070da7eefb8d72acf1eff20e949d1b37f4
Cristina Rodriguez <yosola@gmail.com>
2026-05-14 11:58:53 -0700

    Redirecting 301 for SEO

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,20 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/productos/lustra-muebles",
+        destination: "/productos/lustra-muebles-concentrado",
+        permanent: true,
+      },
+      {
+        source: "/productos/limpia-vidrios-alfombras",
+        destination: "/productos/limpia-vidrios-concentrado",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withNextIntl(nextConfig);
